ad4a2d64aa64a36522148f4f27096f7f06ad8dad
Sarveena <sarveenashrafs@gmail.com>
2019-03-04 17:40:48 +0530

    Add files via upload

---
+++ HibernateAnnotationDemo/src/com/cts/bean/employee.java
@@ -0,0 +1,42 @@
+package com.cts.bean;
+
+import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class employee {
+	@Id
+	private int emp_id;
+	private String emp_name;
+	private String email;
+	private BigInteger mobile;
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public BigInteger getMobile() {
+		return mobile;
+	}
+	public void setMobile(BigInteger mobile) {
+		this.mobile = mobile;
+	}
+
+}
